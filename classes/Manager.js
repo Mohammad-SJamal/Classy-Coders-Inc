15c63fb78ae65fb49de77a2fefbe38322026b481
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2025-05-06 11:34:01 -0500

    finished manager-branch

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,7 +1,17 @@
 class Manager {
+    #employeesManaged
+    constructor(department){
+        this.department = department;
+        this.#employeesManaged = [];
+    }
 
+    getEmployeesManaged(){
+        return this.#employeesManaged;
+    }
 
-
+    addEmployeeManaged(employee){
+        this.#employeesManaged.push(employee);
+    }
     
 }
 
